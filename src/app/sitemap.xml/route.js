549b5258524ca5ea8d03aebaa9810dd1b05a582a
Prashant <prashantambaliya226@gmail.com>
2025-07-29 16:38:15 +0530

    Update route.js

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,43 +1,44 @@
 export async function GET() {
+  const currentDate = new Date().toISOString().split('T')[0];
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
         xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-
+                            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <url>
     <loc>https://www.prashantambaliya.com</loc>
-    <lastmod>2025-01-01</lastmod>
+    <lastmod>${currentDate}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://www.prashantambaliya.com/#about</loc>
-    <lastmod>2025-01-01</lastmod>
+    <lastmod>${currentDate}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>https://www.prashantambaliya.com/#skills</loc>
-    <lastmod>2025-01-01</lastmod>
+    <lastmod>${currentDate}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>
   <url>
     <loc>https://www.prashantambaliya.com/#projects</loc>
-    <lastmod>2025-01-01</lastmod>
+    <lastmod>${currentDate}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
     <loc>https://www.prashantambaliya.com/#experience</loc>
-    <lastmod>2025-01-01</lastmod>
+    <lastmod>${currentDate}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>https://www.prashantambaliya.com/#contact</loc>
-    <lastmod>2025-01-01</lastmod>
+    <lastmod>${currentDate}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
